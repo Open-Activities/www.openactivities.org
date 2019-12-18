3bf60e5c32ac615936d741bae0154a1ae8b1cee0
Sebastian Kürten <sebastian@topobyte.de>
2019-12-18 16:11:32 +0100

    Load data from data repo

--- project/dynamic/src/main/java/org/openactivities/Events.java
+++ project/dynamic/src/main/java/org/openactivities/Events.java
@@ -1,13 +1,20 @@
 package org.openactivities;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.openactivities.model.Event;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import de.topobyte.melon.paths.PathUtil;
+import de.topobyte.system.utils.SystemPaths;
 
 public class Events
 {
@@ -25,14 +32,22 @@ public class Events
 
 	public void load() throws IOException
 	{
-		String startLido = "https://www.lido-berlin.de";
-		String startAstra = "https://www.astra-berlin.de";
-		String linksPattern = "/events/.*";
+		Path repoData = SystemPaths.HOME.resolve("github/Open-Activities/data");
+		Path type1 = repoData.resolve("type1");
+
 		FileCache fileCache = new FileCache();
-		List<Event> eventsLido = load(fileCache, startLido, linksPattern);
-		List<Event> eventsAstra = load(fileCache, startAstra, linksPattern);
-		events.addAll(eventsLido);
-		events.addAll(eventsAstra);
+
+		List<Path> files = PathUtil.list(type1);
+		for (Path file : files) {
+			logger.info("Loading file: " + file);
+			try (InputStream input = Files.newInputStream(file)) {
+				List<String> lines = IOUtils.readLines(input);
+				String start = lines.get(0);
+				String linksPattern = lines.get(1);
+				List<Event> es = load(fileCache, start, linksPattern);
+				events.addAll(es);
+			}
+		}
 	}
 
 	private List<Event> load(FileCache fileCache, String start,
@@ -42,6 +57,7 @@ public class Events
 		Downloader downloader = new Downloader(fileCache, start, linksPattern);
 		downloader.crawl(file -> {
 			try {
+				logger.info("Loading event file: " + file);
 				List<Event> events = EventParsing.parse(file);
 				allEvents.addAll(events);
 			} catch (Throwable e) {
